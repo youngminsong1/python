56cb03340b870c338f67418290f961db44516968
youngminsong1 <127168789+youngminsong1@users.noreply.github.com>
2023-03-28 12:13:34 +0900

    4week

---
+++ python/chap3/2.py
@@ -0,0 +1,31 @@
+'''var="Python"
+print(var[0])
+print(var[0],"  ",var[1],"   ",var[-4],"   ")
+print(len(var))
+print("PYTHON"[0])'''
+'''
+print("Python"[0:5])
+print("Python"[0:])
+print("Python"[2:4])
+print("Python"[:5])
+print("Python"[0:5])
+'''
+a="Python"
+print(a[-5:-1])
+print(a[-4:-1])
+print(a[-6:-3])
+print(a[-6:-1])
+print(a[-len(a):-1])
+
+print(a[:3])
+print(a[:4])
+print(a[1:])
+print(a[3:])
+print(a[:])
+
+str="Monty Python"
+print(len(str))
+print(str[0:5],str[6:],str[6:12])
+print(str[-12:-7],str[-6:],str[-6:0])
+
+
