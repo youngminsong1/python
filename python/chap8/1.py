f92acff78f82bbff0a23c84f40604f5f57575cf9
songyoungmin <songjwo0410@gmail.com>
2023-05-09 12:36:43 +0900

    test

---
+++ python/chap8/1.py
@@ -0,0 +1,18 @@
+'''import math
+import sys
+print(sys.builtin_module_names)
+import numpy as np
+from math import gcd
+print(math.fsum([1,2,3]))
+print(math.gcd(10,20))
+
+arrA=np.array([1,2,3,4,5])
+arrB=np.array([6,7,8,9,10])
+print(arrA+arrB)
+print(arrA-arrB)
+print(arrA*arrB)
+print(arrA/arrB)
+import hello as h
+h.hello()'''
+from songfd import hello2
+hello2.hello()
